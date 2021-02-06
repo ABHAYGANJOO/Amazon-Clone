d05516052609c028556b97cd7760676795d5b72f
ABHAYGANJOO <abhaybbmaster@gmail.com>
2021-02-06 22:27:54 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,6 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfVSbwfb2yPgvePfRv8hX2nyIX1F6yEJ4",
-  authDomain: "clone-52b0c.firebaseapp.com",
-  projectId: "clone-52b0c",
-  storageBucket: "clone-52b0c.appspot.com",
-  messagingSenderId: "999398084192",
-  appId: "1:999398084192:web:5edda07a4713b1d13efd05",
   measurementId: "G-JY1EC9NK3L"
 };
 
